9ef373a6fb180eca5558b367616ace39f2ce0987
Liam P Clancy <learnplaycreate@outlook.com>
2023-07-16 11:23:38 +1000

    I made some changes to the implementation of the platform integrations helpers

--- crates/egui/src/widgets/plot/pm_egui_plot_helpers.rs
+++ crates/egui/src/widgets/plot/pm_egui_plot_helpers.rs
@@ -11,7 +11,7 @@ pub struct PmEguiPlotHelpers {
     pub pattern : Arc<RwLock<Pattern>>, 
     pub drawing_tool : Arc<RwLock<CurrentDrawingTool>>, 
     pub selected_items : Arc<RwLock<SelectedElements>>, 
-    pub platform_integration_methods : Arc<RwLock<PlatformIntegrationMethods>>,
+    pub platform_integration_methods : Arc<PlatformIntegrationMethods>,
     response_drag_delta_detection_limit : f32, // RESPONSE_DRAG_DELTA_DETECTION_LIMIT
     pattern_drawing_live_colour: Color32,
     pattern_measurement_colour: Color32,
@@ -30,7 +30,7 @@ impl Default for PmEguiPlotHelpers {
             pattern :  Arc::new(RwLock::new(Pattern::default())),
             drawing_tool : Arc::new(RwLock::new(CurrentDrawingTool::default())),
             selected_items : Arc::new(RwLock::new(SelectedElements::default())),
-            platform_integration_methods : Arc::new(RwLock::new(PlatformIntegrationMethods::default())),
+            platform_integration_methods : Arc::new(PlatformIntegrationMethods::default()),
             response_drag_delta_detection_limit: 0.0,
             pattern_drawing_live_colour: Color32::GREEN,
             pattern_measurement_colour: Color32::GRAY,
@@ -44,7 +44,7 @@ impl PmEguiPlotHelpers{
         pattern : Arc<RwLock<Pattern>>, 
         drawing_tool : Arc<RwLock<CurrentDrawingTool>>, 
         selected_items : Arc<RwLock<SelectedElements>>, 
-        platform_integration_methods : Arc<RwLock<PlatformIntegrationMethods>>,
+        platform_integration_methods : Arc<PlatformIntegrationMethods>,
         response_drag_delta_detection_limit : f32, // RESPONSE_DRAG_DELTA_DETECTION_LIMIT
         pattern_drawing_live_colour: Color32,
         pattern_measurement_colour: Color32,
@@ -115,7 +115,7 @@ impl PmEguiPlotHelpers{
                     if let Some(selected_piece) = selected_piece_option{
                         if let Err(some_error) = self.drawing_tool.read().unwrap().get_current_tool().piece_selected(drag_pattern_pos, Arc::clone(&self.pattern), Arc::clone(&self.selected_items), selected_piece, render_to_index){
                             //todo - lpc, pass this to the the error handler
-                            (self.platform_integration_methods.read().unwrap().alert_function)(&*some_error.to_string());
+                            (self.platform_integration_methods.alert_function)(&*some_error.to_string());
                         }
                     }
                 }else if select_mode == SelectionModeKind::LayoutPiece{
@@ -123,7 +123,7 @@ impl PmEguiPlotHelpers{
                     if let Some(selected_piece) = selected_piece_option{
                         if let Err(some_error) = self.drawing_tool.read().unwrap().get_current_tool().piece_selected(drag_pattern_pos, Arc::clone(&self.pattern), Arc::clone(&self.selected_items), selected_piece, render_to_index){
                             //todo - lpc, pass this to the the error handler
-                            (self.platform_integration_methods.read().unwrap().alert_function)(&*some_error.to_string());
+                            (self.platform_integration_methods.alert_function)(&*some_error.to_string());
                         }
                     }
                 }else{
@@ -146,12 +146,12 @@ impl PmEguiPlotHelpers{
                     if let Some(element) = closest_element{
                         if let Err(some_error) = self.drawing_tool.read().unwrap().get_current_tool().item_selected(drag_pattern_pos, Arc::clone(&self.pattern), Arc::clone(&self.selected_items), element, render_to_index){
                             //todo - lpc, pass this to the the error handler
-                            (self.platform_integration_methods.read().unwrap().alert_function)(&*some_error.to_string());
+                            (self.platform_integration_methods.alert_function)(&*some_error.to_string());
                         }
                     }else{
                         if let Err(some_error) = self.drawing_tool.read().unwrap().get_current_tool().click_no_selection(drag_pattern_pos, Arc::clone(&self.pattern), Arc::clone(&self.selected_items), render_to_index){
                             //todo - lpc, pass this to the the error handler
-                            (self.platform_integration_methods.read().unwrap().alert_function)(&*some_error.to_string());
+                            (self.platform_integration_methods.alert_function)(&*some_error.to_string());
                         }
                     }
                     set_render_to_index(&self.pattern, render_to_index);
@@ -186,7 +186,7 @@ impl PmEguiPlotHelpers{
                     }
                 }
                 Err(error) => {
-                    (self.platform_integration_methods.read().unwrap().alert_function)(&error.to_string());
+                    (self.platform_integration_methods.alert_function)(&error.to_string());
                     return true;
                 }
             }
